d4c7e035fd85f0d164d516126ebe58b868bc38ec
Jacob Courtney <jacobc@ou.edu>
2019-04-07 14:54:50 -0500

    Basically Done! Just write the copy constructor

--- Project4/main.cpp
+++ Project4/main.cpp
@@ -69,16 +69,11 @@ ostream& operator << (ostream& output, Tree &M) {
 
 //returns the least common ancestor given two nodes
 int Tree::LCA(int node1, int node2) {
-    int levelOfLCA, currentParent1, currentParent2;
-    currentParent1 = node1;
-    currentParent2 = node2;
     
-    levelOfLCA = Level(node1) <= Level(node2) ? node1 - 1:node2 - 1; //DEPRECATED
-    
-    for (int i = Level(currentParent1) <= Level(currentParent2) ? currentParent1 : currentParent2; Parent(i) != -1; i = Parent(i)) {
-        for (int j = Level(currentParent1) >= Level(currentParent2) ? currentParent1 : currentParent2; Parent(j) != -1; j = Parent(j)) {
-            if (currentParent1 == currentParent2){
-                return currentParent1;
+    for (int i = Level(node1) <= Level(node2) ? node1 : node2; Parent(i) != -1; i = Parent(i)) {
+        for (int j = Level(node1) >= Level(node2) ? node1 : node2; Parent(j) != -1; j = Parent(j)) {
+            if (i == j) {
+                return i;
             } 
         }
     }
@@ -100,7 +95,7 @@ int Tree::Parent(int node) {
 void Tree::printChildren(int node) {
     for (int i = 0; i < size() - 1; i++) {
         if (Parent(i) == node) {
-            cout << node << " ";
+            cout << i << " ";
         }
     }
 }
@@ -109,7 +104,7 @@ void Tree::printChildren(int node) {
 void Tree::printSiblings(int node) {
     for (int i = 0; i < size() - 1; i++) {
         if (Parent(i) == Parent(node)) {
-            cout << node << " ";
+            cout << i << " ";
         }
     }
 }
@@ -141,13 +136,106 @@ void Tree::setParent(int node, int parent) {
 void Tree::nodesAtLevel(int level) {
     for (int i = 0; i < size() - 1; i++) {
         if (Level(i) == level) {
-            cout << data[i] << " ";
+            cout << i << " ";
         }
     }
 }
 
+//returns the level of the given node. Recursive
+int Tree::Level(int node) {
+    if (Parent(node) == -1) {
+        return 1;
+    } else {
+        return 1 + Level(Parent(node));
+    }
+}
+
+//returns the height of the tree
+int Tree::height() {
+    int currentHeight = 1;
+    int tallestHeight = 1;
+    int currentNode = 0;
+    for (int i = 0; i < size(); i++) {
+        while (currentNode != -1) {
+            currentNode = Parent(currentNode);
+            currentHeight++;
+        }
+        if (currentHeight > tallestHeight) {
+            tallestHeight = currentHeight;
+        }
+        currentHeight = 1;
+    }
+    return tallestHeight;
+}
+
+//returns the size of the array, which is also equivalent to the number of nodes in the tree
+int Tree::size() {
+    return _size;
+}
+
 int main() {
+    Tree* myTree;
+    int numNodes,node,parent;
     
+    cin >> numNodes;
+    myTree = new Tree(numNodes);
+    
+    for (int i = 0; i < numNodes; i++) {
+        cin >> node >> parent;
+        (*myTree).setParent(node, parent);
+        if (parent == -1) {
+            (*myTree).setRoot(node);
+        }
+    }
+    
+    cout << "The tree that we just read is: " << endl;
+    cout << (*myTree) << endl;
+    
+    /*
+    Tree* newTree = new Tree(*myTree);
+    cout << "The tree that we just copied is: " << endl;
+    cout << (newTree) << endl;
+    */
+     
+    cout << "The root of the tree is: " << (*myTree).Root() << endl;
+    
+    cout << "The least common ancestor of nodes 3 and 8 is: " << (*myTree).LCA (3,8) << endl;
+    cout << "The least common ancestor of nodes is: " << (*myTree).LCA (13,8) << endl;
+    cout << "The least common ancestor of nodes is: " << (*myTree).LCA (13,11) << endl;
+    
+    cout << "The children of node 12 is/are: " << endl; 
+    (*myTree).printChildren(12);
+    cout << endl;
+    
+    cout << "The children of node 10 is/are: " << endl; 
+    (*myTree).printChildren(10);
+    cout << endl;
+    
+    cout << "The siblings of node 3 is/are: " << endl; 
+    (*myTree).printSiblings(3);
+    cout << endl;
+    
+    cout << "The siblings of node 12 is/are: " << endl; 
+    (*myTree).printSiblings(12);
+    cout << endl;
+
+    cout << "The nodes at level 3 is/are: " << endl; 
+    (*myTree).nodesAtLevel(3);
+    cout << endl;
+    
+    cout << "The height of the tree is: " << (*myTree).height() << endl;
+    
+    cout << "The level of node 3 in the tree is: " << (*myTree).Level(3) << endl;
+    cout << "The level of node 12 in the tree is: " << (*myTree).Level(12) << endl;
+    
+    /*
+    cout << "The preorder traversal of the tree is/are" << endl;
+    (*myTree).Preorder();
+    cout << endl;
+    */
+     
+    delete myTree;
+//    delete newTree;
     
     return 0;
 }
