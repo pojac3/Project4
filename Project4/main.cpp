baa6cd82958471461770fd45002b7f3f50c84c86
Jacob Courtney <jacobc@ou.edu>
2019-04-07 22:00:07 -0500

    commented some more and fixed the printSiblings method. still needs some more commenting then im finished

--- Project4/main.cpp
+++ Project4/main.cpp
@@ -15,43 +15,48 @@ using namespace std;
 class Tree {
 public:
     //Project methods
-    Tree();
-    Tree(int numberOfNodes);
-    Tree(Tree const &toBeCopied);
-    virtual ~Tree();
-    friend ostream &operator << (ostream&, Tree &M);
-    int LCA(int node1,int node2);
-    void printParent(int node);
-    int Parent(int node);
-    void printChildren(int node);
-    void printSiblings(int node);
-    int Root();
-    void setRoot(int rootNode);
-    void setParent(int node, int parent);
-    void nodesAtLevel(int level);
-    int Level(int node);
-    int height();
-    //void Preorder();
+    Tree(); //default constructor
+    Tree(int numberOfNodes); //creates a tree with numberOfNodes number of nodes
+    Tree(Tree const &toBeCopied); //copy constructor
+    virtual ~Tree(); //destructor
+    friend ostream &operator << (ostream&, Tree &M); //ostream operator
+    int LCA(int node1, int node2); //finds the least common ancestor and returns that node as an int
+    void printParent(int node); //prints the parent of the specified node to the console
+    int Parent(int node); //returns the parent of the specified node as an int
+    void printChildren(int node); //prints the children of the specified node to the console
+    void printSiblings(int node); //prints the siblings of the specified node to the console
+    int Root(); //returns the root of the tree as an int
+    void setRoot(int rootNode); //sets the root of the tree to the specified node
+    void setParent(int node, int parent); //sets the parent of the specified node to the specified parent
+    void nodesAtLevel(int level); //prints all of the nodes at the specified level to the console
+    int Level(int node); //finds the level of the specified node and returns it as an int
+    int height(); //returns the height of the tree as an int
+    //void Preorder(); //prints the preorder traversal of the tree to the console
     
     //helper methods
-    int size();
+    int size(); //returns the number of nodes in the tree, which is equivalent to the size of the data array, as an int
 protected:
-    int _size;
-    int* data;
+    int _size; //stores the number of nodes in the tree
+    int* data; //main array that stores the tree
 };
 
 /**
  *  C O N S T R U C T O R / D E S T R U C T O R =================================================================== C O N S T R U C T O R / D E S T R U C T O R
  */
+
+//default constructor sets the size of the tree to 0
 Tree::Tree() {
     data = new int[0];
+    _size = 0;
 };
 
+//regular constructor takes in the number of nodes and makes that the size of the tree
 Tree::Tree(int numberOfNodes) {
     data = new int[numberOfNodes];
     _size = numberOfNodes;
 };
 
+//copy constructor takes in another tree and deep copies it
 Tree::Tree(Tree const &toBeCopied) {
     data = new int[toBeCopied._size];
     _size = toBeCopied._size;
@@ -60,7 +65,9 @@ Tree::Tree(Tree const &toBeCopied) {
     }
 };
 
+//destructor basically just deletes the data array
 Tree::~Tree() {
+    delete[] data;
 };
 
 /**
@@ -78,31 +85,42 @@ ostream& operator << (ostream& output, Tree &M) {
 //returns the least common ancestor given two nodes
 int Tree::LCA(int node1, int node2) {
     
+    //weird for loops, yes. i is set to the higher level node, j is set to the lower level node
+    //the condition the loops are checking for is that the parent of the current node isnt the root
+    //the increment increments up to the current nodes parent
     for (int i = Level(node1) <= Level(node2) ? node1 : node2; Parent(i) != -1; i = Parent(i)) {
         for (int j = Level(node1) >= Level(node2) ? node1 : node2; Parent(j) != -1; j = Parent(j)) {
+            //if the two nodes are equivalent, then the LCA has been found. 
             if (i == j) {
+                //this return statement could be set to return either i or j, since they would be equal
                 return i;
             } 
         }
     }
     
+    //defaults to root if for some reason the LCA wasnt found. but it should be found in all cases
     return Root();
 }
 
 //prints the parent of the given node to the console
 void Tree::printParent(int node) {
+    //just a simple cout statement
     cout << data[node];
 }
 
 //returns the parent of the given node as an int
 int Tree::Parent(int node) {
+    //just gets it from the data array
     return data[node];
 }
 
 //prints the children of the given node
 void Tree::printChildren(int node) {
+    //loops through the data array
     for (int i = 0; i < size() - 1; i++) {
+        //if the parent of the current node is the node..
         if (Parent(i) == node) {
+            //then cout that guy!
             cout << i << " ";
         }
     }
@@ -110,8 +128,11 @@ void Tree::printChildren(int node) {
 
 //prints the siblings of the given node
 void Tree::printSiblings(int node) {
+    //loops through the data array
     for (int i = 0; i < size() - 1; i++) {
-        if (Parent(i) == Parent(node)) {
+        //if the parent of the current node is equal to the parent of node (and they arent the same node)..
+        if (Parent(i) == Parent(node) && i != node) {
+            //then cout that guy!
             cout << i << " ";
         }
     }
@@ -119,31 +140,40 @@ void Tree::printSiblings(int node) {
 
 //gives the Root of the tree in int format
 int Tree::Root() {
+    //going to keep track of where we are
     int _root = -1;
     
+    //looping through the data array
     for (int i = 0; i < size() -1; i++) {
-        if (data[i] == -1) {
+        //if the parent of the current node is -1..
+        if (Parent(i) == -1) {
+            //then set that guy equal to _root!
             _root = i;
         }
     }
-    
+    //returning _root
     return _root;
 }
 
 //sets the root of the tree to the specified node
 void Tree::setRoot(int rootNode) {
+    //just setting it to the specified index in the array
     data[rootNode] = -1;
 }
 
 //sets the parent of the node to the specified node
 void Tree::setParent(int node, int parent) {
+    //just setting it to the specified index in the array
     data[node] = parent;
 }
 
 //prints out all the nodes at the specified level
 void Tree::nodesAtLevel(int level) {
+    //looping through the data array
     for (int i = 0; i < size() - 1; i++) {
+        //if the level of the current node is equal to the level were looking for..
         if (Level(i) == level) {
+            //then cout that guy!
             cout << i << " ";
         }
     }
@@ -151,9 +181,12 @@ void Tree::nodesAtLevel(int level) {
 
 //returns the level of the given node. Recursive
 int Tree::Level(int node) {
+    //if we are at the root node..
     if (Parent(node) == -1) {
+        //then the level is equal to 1 so we will return that
         return 1;
     } else {
+        //otherwise add 1 to the level of the parent of that node
         return 1 + Level(Parent(node));
     }
 }
